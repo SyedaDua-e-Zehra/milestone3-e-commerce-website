fdf5d23a8c7c3de8dd32c9342b999533c6bcf126
Syeda Dua e Zehra <syedaduaezehrar@gmail.com>
2025-01-03 05:00:05 -0800

    ok

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -57,8 +57,8 @@ function Cart() {
       <div className="relative grid grid-cols-8 h-screen bg-gray-200">
         <div className="flex flex-col col-span-5 mt-16 ml-20">
           <h2 className="text-xl font-bold">Your shopping Cart</h2>
-          {cart?.map((item:any ) => (
-            <div className="border rounded-md p-3 bg-white shadow-md flex flex-row space-x-4 my-4">
+          {cart?.map((item:any, index:any ) => (
+            <div key={item.id || index} className="border rounded-md p-3 bg-white shadow-md flex flex-row space-x-4 my-4">
               <Image
                 width={80}
                 height={80}
--- src/app/components/HeroBanner.tsx
+++ src/app/components/HeroBanner.tsx
@@ -43,7 +43,7 @@ function HeroBanner() {
           <Image alt="img" src="https://images-eu.ssl-images-amazon.com/images/G/31/img12/home-improvement/HMDGATEWAYS/Aug-HMD---Hero-2_3000x1200-N-2._CB598590461_.jpg" />
         </div>
         <div>
-          <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/Shreyansh/BAU/Unrexc/D70978891_INWLD_BAU_Unrec_Uber_PC_Hero_3000x1200._CB594707876_.jpg" />
+          <Image alt="img" src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/Shreyansh/BAU/Unrexc/D70978891_INWLD_BAU_Unrec_Uber_PC_Hero_3000x1200._CB594707876_.jpg" />
         </div>
         <div>
           <Image alt="img" src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/Softlines_JWL_SH_GW_Assets/Aug_23/MFD/Shoes/unrec/3000._CB598492396_.jpg" />
